a6a2f9607500c7eff9bcf8d6dc61ba566a2af41b
SANSKAR TUNDURWAR <107166157+SANSKAR-TUNDURWAR@users.noreply.github.com>
2023-07-06 19:04:20 +0530

    Add files via upload

---
+++ src/Components/Cards.js
@@ -0,0 +1,37 @@
+import React , {useState} from "react";
+import Card from './Card'
+
+const Cards = (props) => {
+    let courses = props.courses;
+    let category = props.category;
+    
+    const [likedCoures, setLikedCourses] = useState([]);
+
+    const getCourses = () => {
+        if(category === "All"){
+            let allCourses = [];
+            Object.values(courses).forEach( (courseCategory) => {
+                courseCategory.forEach( (course) => {
+                    allCourses.push(course);
+                })
+            })
+            return allCourses;
+        }
+        else{
+            return courses[category];
+        }
+    }
+
+    return(
+        <div className="flex flex-wrap justify-center gap-4 mb-4">
+            
+            {
+                getCourses().map( (course) => {
+                    return <Card key={course.id} course={course} likedCoures={likedCoures} setLikedCourses={setLikedCourses}/>
+                })
+            }
+        </div>
+    )
+}
+
+export default Cards
